b56a5e38043c868700fd68514e9e64ed8b70bf44
Jacopo Sartini <51260522+jacoposartini@users.noreply.github.com>
2019-10-03 16:49:15 +0200

    Update __init__.py

--- SQLite3_HBMQTT_Plugins/__init__.py
+++ SQLite3_HBMQTT_Plugins/__init__.py
@@ -78,14 +78,15 @@ class SQLiteTopicAccessControlPlugin(BaseTopicPlugin):
 					SELECT topic FROM MQTT_topic WHERE mqtt_user_id = '{id}'
 					""")# EDIT WITH YOUR QUERY TO GET THE USER'S ALLOWED TOPICS
 					
-					"""
-					SELECT MQTT_topic.topic 
-					FROM MQTT_authmqtt, MQTT_topic 
-					WHERE MQTT_authmqtt.username = '{username}'
-					AND MQTT_topic.mqtt_user_id = MQTT_authmqtt.id
-					"""
 					for row in cursor.fetchall():
 						allowed_topic = row[0]
 						if self.topic_ac(req_topic, allowed_topic):
 							return True
 			return False
+		
+"""
+SELECT MQTT_topic.topic 
+FROM MQTT_authmqtt, MQTT_topic 
+WHERE MQTT_authmqtt.username = '{username}'
+AND MQTT_topic.mqtt_user_id = MQTT_authmqtt.id
+"""
